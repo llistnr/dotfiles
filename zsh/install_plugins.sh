78c68b3c6b15ff9cea7e3227b495d901cac719cd
Seungho Lee <lxxseunghh@gmail.com>
2023-12-24 03:23:44 +0900

    Add zsh/install_plugins.sh

---
+++ zsh/install_plugins.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+

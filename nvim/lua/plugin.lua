3598a2c51f4c12d6e58d389b45d9da19456772e2
Seungho Lee <lxxseunghh@gmail.com>
2024-08-12 07:37:27 +0900

    Add treesitter plugin to vim

--- nvim/lua/plugin.lua
+++ nvim/lua/plugin.lua
@@ -45,6 +45,7 @@ require('packer').startup(function(use)
     use 'hrsh7th/nvim-cmp'
     use 'hrsh7th/cmp-vsnip'
     use 'hrsh7th/vim-vsnip'
+    use 'nvim-treesitter/nvim-treesitter'
 
     if packer_bootstrap then
         require('packer').sync()

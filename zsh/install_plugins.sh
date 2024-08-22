32496d0c13e6f9958a064b0de19efe6a29402808
Seungho Lee <lxxseunghh@gmail.com>
2024-08-23 07:08:13 +0900

    Fix shebang to use sh

--- zsh/install_plugins.sh
+++ zsh/install_plugins.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
 git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting

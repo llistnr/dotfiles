61a0a204ec02aa47e7dcaa20a6bb1a4ea4bf8ca3
Seungho Lee <lxxseunghh@gmail.com>
2024-08-23 07:15:58 +0900

    Fix dotfiles folder location

--- create_symlink.sh
+++ create_symlink.sh
@@ -13,14 +13,14 @@ rm ~/.zshrc
 rm ~/.oh-my-zsh/themes/seungho.zsh-theme
 
 # create symbolic links
-ln -s ~/dotfiles/git/gitconfig ~/.gitconfig
-ln -s ~/dotfiles/hypr ~/.config/hypr
-ln -s ~/dotfiles/kitty ~/.config/kitty
-ln -s ~/dotfiles/lazygit ~/.config/lazygit
-ln -s ~/dotfiles/nvim ~/.config/nvim
-ln -s ~/dotfiles/rofi ~/.config/rofi
-ln -s ~/dotfiles/tmux ~/.config/tmux
-ln -s ~/dotfiles/waybar ~/.config/waybar
-ln -s ~/dotfiles/zsh/zshrc ~/.zshrc
-ln -s ~/dotfiles/zsh/seungho.zsh-theme ~/.oh-my-zsh/themes/seungho.zsh-theme
+ln -s ~/.dotfiles/git/gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/hypr ~/.config/hypr
+ln -s ~/.dotfiles/kitty ~/.config/kitty
+ln -s ~/.dotfiles/lazygit ~/.config/lazygit
+ln -s ~/.dotfiles/nvim ~/.config/nvim
+ln -s ~/.dotfiles/rofi ~/.config/rofi
+ln -s ~/.dotfiles/tmux ~/.config/tmux
+ln -s ~/.dotfiles/waybar ~/.config/waybar
+ln -s ~/.dotfiles/zsh/zshrc ~/.zshrc
+ln -s ~/.dotfiles/zsh/seungho.zsh-theme ~/.oh-my-zsh/themes/seungho.zsh-theme
 

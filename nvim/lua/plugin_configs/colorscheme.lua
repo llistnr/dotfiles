bb051565f272ccdb84bcd2cbb0059df3446125de
Seungho Lee <lxxseunghh@gmail.com>
2024-08-23 09:39:00 +0900

    Add nvim-treesitter config

--- nvim/lua/plugin_configs/colorscheme.lua
+++ nvim/lua/plugin_configs/colorscheme.lua
@@ -1,3 +1,14 @@
 -- ayu colorscheme
 require('ayu').colorscheme()
 
+-- treesitter
+require'nvim-treesitter.configs'.setup {
+    ensure_installed = { "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "python" },
+    sync_install = false,
+    auto_install = false,
+    highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+    },
+}
+
